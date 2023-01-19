fad95b6bf1bff01d841b0d9b43f1ea7dc72a01a1
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-01-19 11:56:29 +0300

    Business katmanında Fluent Validation'lar oluşturuldu, EF Repository oluşturuldu, Tema projeye dahil edildi, UI katmanında PartialViewler oluşturuldu, Blog/Index Tasarımı bitirildi.

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,70 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog t)
+        {
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Delete(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBlog(Blog t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Find(id);
+        }
+
+        public List<Blog> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+
+           
+        }
+
+        public void Update(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlog(Blog t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}

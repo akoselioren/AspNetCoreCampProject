b59fb4b198f47fa06a9bdc0286d1fcd14f12af05
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-01-27 09:18:45 +0300

    Ajax ve Identity kullanımı yapıldı.
    Projede Ajax ile CRUD işlemleri yapıldı,
    Bilgi amaçlı olarak projeden bağımsız Swagger ve Postman'de veriler aynı şekilde CRUD işlemleri olarak kullanıldı,
    API'ler ile CRUD işlemleri yapıldı,
    Identity'ye giriş yapıldı Identity nuGet packages projeye dahil edildi.

--- CoreCampProject/Areas/Admin/Controllers/WriterController.cs
+++ CoreCampProject/Areas/Admin/Controllers/WriterController.cs
@@ -3,6 +3,7 @@ using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreCampProject.Areas.Admin.Controllers
 {
@@ -18,6 +19,34 @@ namespace CoreCampProject.Areas.Admin.Controllers
             var jsonWriters = JsonConvert.SerializeObject(writers);
             return Json(jsonWriters);
         }
+
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter=writers.FirstOrDefault(x=>x.Id == writerid);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters=JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(writer);
+            return Json(writer);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
